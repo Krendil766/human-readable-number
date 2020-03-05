cabb30b5c992617b21c32420742575432a1589dc
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-06 00:40:43 +0300

    feat:algoritm task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 module.exports = function toReadable(number) {
-        let array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        if (number < 20) {
-            console.log(array[number]);
+    let array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    if (number < 20) {
+        console.log(array[number]);
 
+    }
+    let ty = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+        'eighty', 'ninety'
+    ]
+    if (number < 20) {
+        console.log(arr[number]);
+    }
+    if (20 < number < 100) {
+        if (number % 10 == 0) {
+            console.log(ty[number / 10])
+        } else {
+            let newN = Math.floor(number / 10);
+            console.log(ty[newN] + " " + arr[number % 10])
         }
-        let ty = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
-            'eighty', 'ninety'
+    }
+    if (number > 99) {
+        if (number % 100 == 0) {
+            console.log(`${arr[number / 100]} hundred`);
+        } else if (number % 10 == 0) {
+            console.log(`${arr[Math.floor(number / 100)]} hundred ${ty[(number / 10) % 10]}`)
+        } else if (number % 100 < 20) {
+            console.log(`${arr[Math.floor(number / 100)]} hundred ${arr[number % 100]}`)
+        } else {
+            console.log(`${arr[Math.floor(number / 100)]} hundred ${ty[(Math.floor(number / 10)) % 10]} ${arr[number % 10]}`)
         }
+    }
+}
