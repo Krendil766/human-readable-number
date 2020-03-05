c2911f9bf2aaea28bc9d6b8e8170af1bd099e0ad
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-06 00:56:34 +0300

    refactor:formatting

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 module.exports = function toReadable(number) {
-    let array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    if (number < 20) {
-        console.log(array[number]);
-
-    }
+    let arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let ty = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
         'eighty', 'ninety'
     ]
     if (number < 20) {
-        console.log(arr[number]);
+        return (arr[number]);
     }
-    if (20 < number < 100) {
+    if (number >= 20 && number < 100) {
         if (number % 10 == 0) {
-            console.log(ty[number / 10])
+            return (ty[number / 10])
         } else {
             let newN = Math.floor(number / 10);
-            console.log(ty[newN] + " " + arr[number % 10])
+            return (ty[newN] + " " + arr[number % 10])
         }
     }
     if (number > 99) {
         if (number % 100 == 0) {
-            console.log(`${arr[number / 100]} hundred`);
+            return (`${arr[number / 100]} hundred`);
         } else if (number % 10 == 0) {
-            console.log(`${arr[Math.floor(number / 100)]} hundred ${ty[(number / 10) % 10]}`)
+            return (`${arr[Math.floor(number / 100)]} hundred ${ty[(number / 10) % 10]}`)
         } else if (number % 100 < 20) {
-            console.log(`${arr[Math.floor(number / 100)]} hundred ${arr[number % 100]}`)
+            return (`${arr[Math.floor(number / 100)]} hundred ${arr[number % 100]}`)
         } else {
-            console.log(`${arr[Math.floor(number / 100)]} hundred ${ty[(Math.floor(number / 10)) % 10]} ${arr[number % 10]}`)
+            return (`${arr[Math.floor(number / 100)]} hundred ${ty[(Math.floor(number / 10)) % 10]} ${arr[number % 10]}`)
         }
     }
+
 }
