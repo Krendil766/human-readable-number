175b8a47260acee6bcb26bb71e53c665cca02a79
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-05 23:41:53 +0300

    feat: add array string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+        let array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+        if (number < 20) {
+            console.log(array[number]);
+
+        }
+        let ty = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+            'eighty', 'ninety'
+        }
